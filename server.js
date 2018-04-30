83c44b91bada920a3aaddeccb587d7e8053a60dc
Soňa Leitmanová <sona.leitmanova@gmail.com>
2018-04-30 15:00:13 +0200

    Update server.js
    todo

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-
+/* TODO 
 let express = require('express');
 let cors = require('cors');
 let app = express();
-/*
+
 app.use(cors());
 
 app.use(function (req, res, next) {
@@ -10,7 +10,6 @@ app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
-*/
 
 app.get('/', function (request, response) {
     let html = '<h1>hello</h1>';
@@ -21,3 +20,4 @@ let PORT = 3000;
 app.listen(PORT, function () {
     console.log('http://localhost:' + PORT);
 });
+*/
